903ce0891c51ca78a4b4885679fac268243ab40a
yena choi <geniusyena332@gmail.com>
2023-01-07 19:52:23 +0900

    FIX : HTTP exception filter 수정 #29
    Changes to be committed:
    	modified:   project/src/http-exception.filter.ts
    	modified:   project/src/main.ts
    
    RESOLVE : #29

--- project/src/main.ts
+++ project/src/main.ts
@@ -7,16 +7,20 @@ import { HttpExceptionFilter } from './http-exception.filter';
 
 declare const module: any;
 
+// CORS 설정 추가해야 함!!
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, { cors: true });
     const port = process.env.PORT || 3000;
-    app.useGlobalFilters(new HttpExceptionFilter())
-    app.useGlobalPipes(new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-        transform: true,
-      }));
+    app.useGlobalFilters(new HttpExceptionFilter());
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true,
+        }),
+    );
 
+    // swagger 설정(update할 때마다 버전 올려주기)
     const config = new DocumentBuilder()
         .setTitle('가치마인드 API 명세')
         .setDescription('가치마인드 HTTP API 명세서')
@@ -26,7 +30,7 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
-    await app.listen(3000);
+    await app.listen(port);
     console.log(`listening on port ${port}`);
 
     if (module.hot) {
