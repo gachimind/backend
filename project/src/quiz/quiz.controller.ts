5df6efd12bc2f1946b9b128a10174de05cfc5099
DY <p930601@gmail.com>
2023-01-23 16:26:10 +0700

    FEAT : 1페이지 크롤링 성공, 페이지마다 크롤링+DB 저장 구현 필요

--- project/src/quiz/quiz.controller.ts
+++ project/src/quiz/quiz.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
-import { QuizService } from 'src/quiz/quiz.service';
+import { QuizService } from './quiz.service';
 
 @Controller('quiz')
 export class QuizController {
     constructor(private quizService: QuizService) {}
 
     @Get()
-    quizController() {
-        return this.quizService.getDataViaPuppeteer();
+    QuizController() {
+        const pageNum = 2500;
+        for (let index = 1; index < pageNum; index++) {
+            const pageNumber: number = pageNum[index];
+            return this.quizService.getData(pageNumber);
+        }
     }
 }
--- project/src/quiz/quiz.service.ts
+++ project/src/quiz/quiz.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import * as puppeteer from 'puppeteer';
+import * as cheerio from 'cheerio';
 
 @Injectable()
 export class QuizService {
-    async getData(pageNum: number) {
-        const URL = `https://terms.naver.com/list.naver?cid=42344&categoryId=42344&page=${pageNum}`;
+    async getData(pageNumber: number) {
+        const URL = `https://terms.naver.com/list.naver?cid=42344&categoryId=42344&so=st1.dsc&viewType=&categoryType=&page=${pageNumber}`;
         const browser = await puppeteer.launch({
             // ignoreHTTPSErrors: true,
             // setRequestInterception 메소드 이용할 때 에러 방지
-            headless: false,
+            headless: true,
             // 처음에는 false 설정해두고 브라우저를 직접 눈으로 보면서 실행되는 부분 확인 가능하고 나중에는 true로 바꿀 것
         });
         const page = await browser.newPage();
@@ -18,32 +19,20 @@ export class QuizService {
             // 최소 500 ms 동안 2개 이상 네트워크 연결이 없을 때 탐색 완료된 것으로 간주
         });
 
-        const results = await page.evaluate(() => {
-            const propertyList = [];
+        const content = await page.content();
+        const $ = cheerio.load(content);
+        const lists = $('#content > div.list_wrap > ul > ');
 
-            // document.querySelectorAll(
-            //   'https://terms.naver.com/list.naver?cid=42344&categoryId=42344',
-            // );
+        const data = lists.each((index, list) => {
+            const word = $(list).find('div > div.subject > strong> a:nth-child(1)').html();
+            const wordExp = word.replace(/[^ㄱ-ㅎ|가-힣|'']/g, '');
+            const hint = $(list).find('div > p').html();
+            const hintExp = hint.replace(/[^a-z|A-Z|0-9|ㄱ-ㅎ|가-힣|' '|「」|()]/g, '');
 
-            const data = {
-                word: document.querySelector(
-                    '#content > div.list_wrap > ul > li:nth-child(1) > div > div.subject > strong > a:nth-child(1)',
-                )?.textContent,
-                hint: document.querySelector(
-                    '#content > div.list_wrap > ul > li:nth-child(1) > div > p',
-                )?.textContent,
-            };
-
-            const wordExp = data.word;
-            const wordFinal = wordExp.replace(/[^ㄱ-ㅎ|가-힣|' ']/g, '');
-            const hintExp = data.hint;
-            const hintFinal = hintExp.replace(/[^a-z|A-Z|0-9|ㄱ-ㅎ|가-힣|' '|「」|()]/g, '');
-
-            propertyList.push(wordFinal, hintFinal);
-            return propertyList;
+            console.log({ index, wordExp, hintExp });
         });
         await browser.close();
-        return results;
+        //     return results;
     }
 }
 
@@ -51,9 +40,13 @@ export class QuizService {
 // https://terms.naver.com/list.naver?cid=42344&categoryId=42344&page=2 >>> 컴퓨터인터넷IT용어대사전 (페이지 구분)
 // https://terms.naver.com/entry.naver?docId=2038502&cid=42344&categoryId=42344 >>> 컴퓨터인터넷IT용어대사전 (단어 구분)
 
-// 단어
-// document.querySelector('#content > div.list_wrap > ul > li:nth-child(1) > div > div.subject > strong > a:nth-child(1)').innerHTML
+//////////////////////////////////////////////////
 
-// 설명
-// document.querySelector('#size_ct > div:nth-child(1) > dl > dd:nth-child(2)').innerText
-// document.querySelector('#content > div.list_wrap > ul > li:nth-child(1) > div > p').innerText
+// #content > div.list_wrap > ul > li:nth-child(1)
+// #content > div.list_wrap > ul > li:nth-child(2)
+
+// #content > div.list_wrap > ul > li:nth-child(1) > div > div.subject > strong
+// #content > div.list_wrap > ul > li:nth-child(1) > div > div.subject > strong > a:nth-child(1)
+// #content > div.list_wrap > ul > li:nth-child(1) > div > p
+
+//////////////////////////////////////////////////
