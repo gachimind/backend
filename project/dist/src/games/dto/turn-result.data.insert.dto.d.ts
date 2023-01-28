11543e933ad0a4d558085f1b0c181124afaad532
DY <p930601@gmail.com>
2023-01-28 17:43:00 +0700

    CHORE : 빌드완료

---
+++ project/dist/src/games/dto/turn-result.data.insert.dto.d.ts
@@ -0,0 +1,10 @@
+export declare class TurnResultDataInsertDto {
+    gameResultInfo: number;
+    roomId: number;
+    turn: number;
+    userId: number;
+    nickname: string;
+    score: number;
+    keyword: string;
+    isSpeech: boolean;
+}
