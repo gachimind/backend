cedf511f874b96bef214025d8cc5eb97be258929
yena choi <geniusyena332@gmail.com>
2023-02-01 14:14:11 +0900

    timzone test

--- project/dist/src/games/util/today.date.constructor.js
+++ project/dist/src/games/util/today.date.constructor.js
@@ -2,8 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getTodayDate = void 0;
 function getTodayDate() {
-    const date = new Date();
+    const date = new Date('Z');
     console.log('date :', date);
+    console.log('ISOString :', date.toISOString());
     const dateString = date.toDateString();
     const newDate = new Date(`${dateString} 0:00:00`);
     return newDate;
--- project/src/games/util/today.date.constructor.ts
+++ project/src/games/util/today.date.constructor.ts
@@ -1,6 +1,7 @@
 export function getTodayDate() {
-    const date = new Date();
+    const date = new Date('Z');
     console.log('date :', date);
+    console.log('ISOString :', date.toISOString());
 
     const dateString = date.toDateString();
     const newDate = new Date(`${dateString} 0:00:00`);
