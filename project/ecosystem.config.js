d60f024a39fdfa3f75afa41c940c79432c3aec10
DY <p930601@gmail.com>
2023-02-03 20:18:53 +0700

    CHORE : pm2 인스턴스 갯수 수정

--- project/ecosystem.config.js
+++ project/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'app',
     script: './dist/src/main.js',
-    instances: 'max', // 몇개 열건지 설정 필수
+    instances: 3, // 몇개 열건지 설정 필수
     exec_mode: 'cluster',
   }]
 }
