076387f59d8b0e1c63732e4d04e9420495a29edd
yena choi <geniusyena332@gmail.com>
2023-02-03 22:44:37 +0900

    FIX : 처음 가입하는 유저의 닉네임 공백 제거 #hotfix
    Changes to be committed:
    	modified:   src/users/auth/kakao.strategy.ts

--- project/ecosystem.config.js
+++ project/ecosystem.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  apps: [{
-    name: 'app',
-    script: './dist/src/main.js',
-    instances: 3, // 몇개 열건지 설정 필수
-    exec_mode: 'cluster',
-  }]
-}
+    apps: [
+        {
+            name: 'app',
+            script: './dist/src/main.js',
+            instances: 3, // 몇개 열건지 설정 필수
+            exec_mode: 'cluster',
+        },
+    ],
+};
